9f2f0d2fd47ef78da6bdf1ff719c5493cc311d64
shanecranor <shanecranor@mines.edu>
2020-11-26 02:45:58 -0700

    added a bunch of shit, its way better now

--- Shane Website V1/scripts/index.js
+++ Shane Website V1/scripts/index.js
@@ -1,94 +1,205 @@
+//GLOBAL VARS
+let app = null;
+let canvas = null;
+let groundGraphic = null;
+let groundLineGraphic = null;
+let groundCoverGraphic = null;
+let starsGraphic = null;
+let starShieldGraphic = null;
+let fogGraphic = null;
+let time = 0;
 window.onload = function(){
+	canvas = document.getElementById("backgroundCanvas");
 	document.getElementById("code").addEventListener("mouseover", mouseOverCode);
 	document.getElementById("code").addEventListener("mouseout", mouseOutCode);
+	init();
 	app.renderer.resize(window.innerWidth, window.innerHeight);
-}
-function mouseOverCode() {
-	document.getElementById("h2Container").style.fontFamily = "Courier New,Courier,monospace";
 
 }
-function mouseOutCode() {
-  document.getElementById("h2Container").style.fontFamily = "Inter, sans-serif";
 
+
+function init(){
+	PIXI.utils.skipHello();
+	app = new PIXI.Application({
+		view: canvas,
+		width: window.innerWidth,
+		height: window.innerHeight,
+		resolution: window.devicePixelRatio,
+		autoDensity: true,
+		antialias: true
+	});
+	starsGraphic = new PIXI.Graphics();
+	starShieldGraphic = new PIXI.Graphics();
+	groundCoverGraphic = new PIXI.Graphics();
+	groundGraphic = new PIXI.Graphics();
+	groundLineGraphic = new PIXI.Graphics();
+	fogGraphic = new PIXI.Graphics();
+	let blurFilter = new PIXI.filters.BlurFilter();
+	let blurFilter2 = new PIXI.filters.BlurFilter();
+	let blurFilter3 = new PIXI.filters.KawaseBlurFilter(60,10,true);
+	let blurFilter4 = new PIXI.filters.KawaseBlurFilter(100,5,true);
+	let glitchFilter = new PIXI.filters.GlitchFilter({
+		slices: 10,
+		offset: 10,
+		fillMode: 2 //LOOP
+	});
+	let CRTFilter = new PIXI.filters.CRTFilter({
+		vignetting: .1
+	});
+	blurFilter.blur = 25;
+	blurFilter.quality = 5;
+	blurFilter2.blur = 1;
+
+	app.stage.padding = 300;
+	fogGraphic.padding = 300;
+	starShieldGraphic.filters = [blurFilter4];
+	groundGraphic.filters = [blurFilter];
+	groundLineGraphic.filters = [blurFilter2];
+	fogGraphic.filters = [blurFilter3];
+
+	//app.stage.filters = [CRTFilter];
+	//groundGraphic.blendMode = PIXI.BLEND_MODES.ADD;
+	app.stage.addChild(starsGraphic);
+	app.stage.addChild(starShieldGraphic);
+	app.stage.addChild(groundCoverGraphic);
+	app.stage.addChild(groundGraphic);
+	app.stage.addChild(fogGraphic);
+	app.stage.addChild(groundLineGraphic);
+
+	app.ticker.add(animationloop);
+
+	time = 0; //GLOBAL TIME VAR
+
+	generateStars(200,3);
 }
-const canvas = document.getElementById("backgroundCanvas");
 
-PIXI.utils.skipHello();
-
-const app = new PIXI.Application({
-	view: canvas,
-	width: window.innerWidth,
-	height: window.innerHeight,
-	resolution: window.devicePixelRatio,
-	autoDensity: true,
-	antialias: true
-});
-let graphic = new PIXI.Graphics();
-app.stage.addChild(graphic);
-
-app.ticker.add(animationloop);
-
-var time = 0; //GLOBAL TIME VAR
-var width = app.screen.width;
-var height = app.screen.height;
-generateStars(200,3);
-function animationloop() {
-	width = app.screen.width;
-	height = app.screen.height;
-	graphic.clear();
-	graphic.alpha = 1;
-
-	time += 0.1;
-	//if()
-	for(var i = 20; i > 1; i/=1.7){
-		//graphic.alpha = 1/i;
-		graphic.lineStyle(i+.5, hslToHex(.82, 1, (1/i) ));
-		matrixGround(200,10,(height/2)-(height/5), 6, 6, 0,-.1);
-	}
-
-}
 function generateStars(num, size){
-	var star1Texture = PIXI.Texture.from('img/star1.gif');
-	var starsTexture = PIXI.Texture.from('img/stars-tiled.jpg');
-	r = mulberry32(223452); //seed random number gen, generate numbers by calling r()
+	starsGraphic.clear();
+	for (var i = starsGraphic.children.length - 1; i >= 0; i--) {
+		starsGraphic.removeChild(starsGraphic.children[i]);
+	};
+	var width = app.screen.width;
+	var height = app.screen.height;
+	var star1Texture = PIXI.Texture.from('img/stars-tiled.jpg');
+	var starsTexture = PIXI.Texture.from('img/stars-tiled-2-min (1).png');
+	r = mulberry32(6969); //seed random number gen, generate numbers by calling r()
 	for(var x = 0; x < width; x+=(55+(r()*1))*3){
 		for(var y = 0; y < height; y+=3*(95+(r()*1))){
-		//var x = r()*width;
-		//var y = r()*height;
-		var s = Math.pow(r(), 500)*size+.7; //math.pow makes larger stars less common
-		var star = new PIXI.Sprite(starsTexture);
-		star.position.x = x+r()*40;
-		star.position.y = y+r()*10;
-		star.blendMode = PIXI.BLEND_MODES.ADD;
-		star.scale.set(.63);
-		star.anchor.set(0.5);
-		star.alpha = .5;
-		star.angle = r() *  360;
-		app.stage.addChild(star);
-		/*   //OLD circle based rendering
+			//var x = r()*width;
+			//var y = r()*height;
+			//var s = Math.pow(r(), 500)*size+.7; //math.pow makes larger stars less common
+			;
+
+			let starX = x+r()*40;
+			let starY = y+r()*10;
+			let star = null;
+			//if(Math.abs(starY - height/2) > 400){
+			star = new PIXI.Sprite(starsTexture);
+			star.scale.set(.35);
+			star.anchor.set(.8);
+			star.alpha = 1;
+			star.angle = r() *  360;
+			star.position.x = starX;
+			star.position.y = starY;
+			if(r() < 0.5 && Math.abs(star.position.y - height/2) > 300) {
+				star.blendMode = PIXI.BLEND_MODES.ADD;
+			}
+
+			starsGraphic.addChild(star);
+			/*   //OLD circle based rendering
 		graphic.lineStyle(0);
 		graphic.alpha = 1;
 		graphic.beginFill(0xFFFFFF);
 		graphic.drawCircle(x,y,s);
 		graphic.endFill();
 		*/
-
 		}
 	}
+
+	starShieldGraphic.clear();
+	starShieldGraphic.beginFill(0x000000);
+	starShieldGraphic.alpha = .9
+	starShieldGraphic.drawEllipse(width/2,6*height/8,width/1.5,height/2);
+	groundCoverGraphic.clear();
+	groundCoverGraphic.beginFill(0x000000);
+	groundCoverGraphic.alpha = 1;
+	groundCoverGraphic.drawRect(0,height-((height/2)-(height/5)), width, height+100)
 	//graphic.endFill();
 }
 
-function mulberry32(a) {
-    return function() {
-      var t = a += 0x6D2B79F5;
-      t = Math.imul(t ^ t >>> 15, t | 1);
-      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-      return ((t ^ t >>> 14) >>> 0) / 4294967296;
-    }
+function drawBottomGrid() {
+	var width = app.screen.width;
+	var height = app.screen.height;
+	groundGraphic.clear();
+	groundGraphic.alpha = 1;
+	groundLineGraphic.clear();
+	groundLineGraphic.alpha = 1;
+
+	time += 0.1;
+	//if()
+	for (let i = 20; i > 1; i /= 2) {
+		//graphic.alpha = 1/i;
+		//let color = hslToHex(.82, 1, (1/i) );
+		let color = hslToHex(.82, .1, (1 / i) / 2 + .5);
+		//groundGraphic.lineStyle(i+.5, color, );
+		groundLineGraphic.lineStyle({
+			width: i + .5,
+			color: color,
+			alignment: 0.5,
+			alpha: Math.sqrt(1 / i),
+			cap: 'round',
+		})
+		matrixGround(300, 20, (height / 2) - (height / 5), 6, 6, 0, -.1, groundLineGraphic);
+	}
+	let i = 20;
+	let color = hslToHex(.82, .6, (1 / i) / 2 + .32);
+	groundGraphic.lineStyle({
+		width: i * 1.5,
+		color: color,
+		alignment: 0.5,
+		alpha: 1,
+		cap: 'round',
+	})
+	matrixGround(300, 20, (height / 2) - (height / 5), 6, 6, 0, -.1, groundGraphic);
 }
-function matrixGround(xdiv, ydiv, top, aspectX, aspectY, speedX, speedY){
+
+function drawFog() {
+	var width = app.screen.width;
+	var height = app.screen.height;
+	fogGraphic.clear();
+	fogGraphic.alpha = 1;
+	let o = 40; //overlap constant
+	fogGraphic.beginFill(hslToHex(.6,1,.4),.25);
+	fogGraphic.drawEllipse(3*width/4,height/2, width/3.5,height/2);
+	fogGraphic.beginFill(hslToHex(.6,1,.4),.2);
+	fogGraphic.drawRect(width/2-width/o,height/40,3*width/8+width/o,height-height/20);
+
+	fogGraphic.beginFill(hslToHex(.83,1,.4),.25);
+	fogGraphic.drawEllipse(width/4,height/2, width/3.5,height/2);
+
+	fogGraphic.beginFill(hslToHex(.83,1,.4),.2);
+	fogGraphic.drawRect(width/8,height/40,3*width/8+width/o,height-height/20);
+	//fogGraphic.blendMode = PIXI.BLEND_MODES.ADD;
+}
+
+function animationloop() {
+	drawBottomGrid();
+	drawFog();
+}
+
+function mulberry32(a) {
+	return function() {
+		var t = a += 0x6D2B79F5;
+		t = Math.imul(t ^ t >>> 15, t | 1);
+		t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+		return ((t ^ t >>> 14) >>> 0) / 4294967296;
+	}
+}
+function matrixGround(xdiv, ydiv, top, aspectX, aspectY, speedX, speedY, graphic){
+	var width = app.screen.width;
+	var height = app.screen.height;
 	/***vertical line loop:***/
-	//Calculate the extra lines needed to compensate for the X aspect not making it all the way to the edge of the canvas
+		//Calculate the extra lines needed to compensate for the X aspect not making it all the way to the edge of the canvas
 	var extra = Math.abs(width - (width * aspectX)) / 2;
 	var timeModifierX = (time * speedX) % xdiv
 
@@ -98,7 +209,7 @@ function matrixGround(xdiv, ydiv, top, aspectX, aspectY, speedX, speedY){
 		//use arious maths to keep the perspective centered
 		var x0 = (( x - (width/2)) / aspectX ) + (width/2);
 		//draw vertical lines
-		line(x0,	height-top, x,	height);
+		line(x0,	height-top, x,	height, graphic);
 	}
 	/***horizontal line loop:***/
 	var timeModifierY = -1 * ((time * speedY) % 1);
@@ -107,7 +218,7 @@ function matrixGround(xdiv, ydiv, top, aspectX, aspectY, speedX, speedY){
 		//makes everything further away as it gets closer to the bottom of the screen
 		var newY = (height-top) + (y * ydiv * (y) / aspectY);
 		//draw horizontal lines
-		line(0, newY, width, newY)
+		line(0, newY, width, newY, graphic)
 	}
 }
 function hslToHex(h, s, l) {
@@ -135,7 +246,7 @@ function hslToHex(h, s, l) {
 
 	return PIXI.utils.rgb2hex([r, g, b]);
 }
-function line(x0,y0,x1,y1){
+function line(x0,y0,x1,y1, graphic){
 	graphic.moveTo(x0,y0);
 	graphic.lineTo(x1,y1);
 }
@@ -144,4 +255,13 @@ function sin(x){
 }
 window.addEventListener("resize", function () {
 	app.renderer.resize(window.innerWidth, window.innerHeight);
+	generateStars(200,3);
 });
+function mouseOverCode() {
+	document.getElementById("h2Container").style.fontFamily = "Courier New,Courier,monospace";
+
+}
+function mouseOutCode() {
+	document.getElementById("h2Container").style.fontFamily = "Inter, sans-serif";
+
+}
